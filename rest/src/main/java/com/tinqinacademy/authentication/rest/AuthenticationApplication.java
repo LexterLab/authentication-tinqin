010d3f738a9285eeead422df43cc246524725520
moldovexc <alexanderparpulansky@gmail.com>
2024-08-04 17:56:18 +0300

    Add missing entry point to spring boot application

--- rest/src/main/java/com/tinqinacademy/authentication/rest/AuthenticationApplication.java
+++ rest/src/main/java/com/tinqinacademy/authentication/rest/AuthenticationApplication.java
@@ -1,5 +1,6 @@
 package com.tinqinacademy.authentication.rest;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,4 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = {"com.tinqinacademy.authentication.persistence.repositories"})
 @EnableFeignClients(basePackages = {"com.tinqinacademy.authentication"})
 public class AuthenticationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationApplication.class, args);
+    }
 }
