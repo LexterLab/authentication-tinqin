b4e1d33ea3bbb4c34eb6a51157733a6060f1192c
moldovexc <alexanderparpulansky@gmail.com>
2024-08-07 00:10:49 +0300

    Add user not found with email message constant

--- api/src/main/java/com/tinqinacademy/authentication/api/Messages.java
+++ api/src/main/java/com/tinqinacademy/authentication/api/Messages.java
@@ -6,4 +6,5 @@ public class Messages {
     public static final String EXPIRED_JWT_TOKEN = "Session expired. You have been signed out";
     public static final String UNSUPPORTED_JWT_TOKEN = "Unsupported JWT Token";
     public static final String JWT_CLAIM_EMPTY = "JWT claim string is empty";
+    public static final String USER_NOT_FOUND_WITH_EMAIL = "User not found with email: ";
 }
