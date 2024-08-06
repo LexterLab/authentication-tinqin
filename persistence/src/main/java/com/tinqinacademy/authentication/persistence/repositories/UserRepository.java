3f9503944d98007241c641e2b9b2a2ac2ac04205
moldovexc <alexanderparpulansky@gmail.com>
2024-08-06 21:53:07 +0300

    Add user repository with required queries

---
+++ persistence/src/main/java/com/tinqinacademy/authentication/persistence/repositories/UserRepository.java
@@ -0,0 +1,20 @@
+package com.tinqinacademy.authentication.persistence.repositories;
+
+import com.tinqinacademy.authentication.persistence.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findUserByEmailIgnoreCase(String email);
+    Boolean existsByEmailIgnoreCase(String email);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User u SET u.isVerified = TRUE WHERE u.email = ?1")
+    void confirmEmail(String email);
+}
