9c631b33d46f4bd2e22cb1678bf2b9c672751f1a
moldovexc <alexanderparpulansky@gmail.com>
2024-08-06 23:48:11 +0300

    Add ERROR messages for jwt operations

--- api/src/main/java/com/tinqinacademy/authentication/api/Messages.java
+++ api/src/main/java/com/tinqinacademy/authentication/api/Messages.java
@@ -2,4 +2,8 @@ package com.tinqinacademy.authentication.api;
 
 public class Messages {
     public static final String RESOURCE_NOT_FOUND = "%s not found with %s : '%s'";
+    public static final String INVALID_JWT_TOKEN = "Invalid JWT Token";
+    public static final String EXPIRED_JWT_TOKEN = "Session expired. You have been signed out";
+    public static final String UNSUPPORTED_JWT_TOKEN = "Unsupported JWT Token";
+    public static final String JWT_CLAIM_EMPTY = "JWT claim string is empty";
 }
