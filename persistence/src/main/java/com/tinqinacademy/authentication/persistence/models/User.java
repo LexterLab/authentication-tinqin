980a2bbf8a8fce94f34be54bff16f6673d552a93
moldovexc <alexanderparpulansky@gmail.com>
2024-08-06 21:46:00 +0300

    Add user entity

---
+++ persistence/src/main/java/com/tinqinacademy/authentication/persistence/models/User.java
@@ -0,0 +1,49 @@
+package com.tinqinacademy.authentication.persistence.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Generated;
+
+
+import java.util.Set;
+import java.util.UUID;
+
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String phoneNo;
+
+    @Generated
+    @ColumnDefault(value = "false")
+    private Boolean isVerified;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id") )
+    private Set<Role> roles;
+}
