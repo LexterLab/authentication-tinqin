9f6cd36eadc6a5882b84e271a60f0766bee80934
moldovexc <alexanderparpulansky@gmail.com>
2024-08-07 13:10:44 +0300

    Send confirmation email operation

---
+++ api/src/main/java/com/tinqinacademy/authentication/api/operations/sendconfirmemail/SendConfirmEmailOutput.java
@@ -0,0 +1,10 @@
+package com.tinqinacademy.authentication.api.operations.sendconfirmemail;
+
+import com.tinqinacademy.authentication.api.base.OperationOutput;
+import lombok.Builder;
+import lombok.ToString;
+
+@Builder
+@ToString
+public class SendConfirmEmailOutput implements OperationOutput {
+}
