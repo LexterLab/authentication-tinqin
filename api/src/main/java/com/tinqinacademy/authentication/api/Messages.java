96b6203f9ee18294944ac748d737407dd1a1ac97
moldovexc <alexanderparpulansky@gmail.com>
2024-08-07 13:11:50 +0300

    Add error messages

--- api/src/main/java/com/tinqinacademy/authentication/api/Messages.java
+++ api/src/main/java/com/tinqinacademy/authentication/api/Messages.java
@@ -6,5 +6,7 @@ public class Messages {
     public static final String EXPIRED_JWT_TOKEN = "Session expired. You have been signed out";
     public static final String UNSUPPORTED_JWT_TOKEN = "Unsupported JWT Token";
     public static final String JWT_CLAIM_EMPTY = "JWT claim string is empty";
-    public static final String USER_NOT_FOUND_WITH_EMAIL = "User not found with email: ";
+    public static final String USER_NOT_FOUND_WITH_USERNAME = "User not found with username: ";
+    public static final String EMAIL_ALREADY_EXISTS = "Email already exists: %s";
+    public static final String USERNAME_ALREADY_EXISTS = "Username already exists: %s";
 }
