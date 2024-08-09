eb6fe1efb6da586b768b5bdb7bb84f28863e67ca
moldovexc <alexanderparpulansky@gmail.com>
2024-08-09 11:09:44 +0300

    add export for get user op

--- restexport/src/main/java/com/tinqinacademy/authentication/restexport/AuthenticationClient.java
+++ restexport/src/main/java/com/tinqinacademy/authentication/restexport/AuthenticationClient.java
@@ -3,11 +3,13 @@ package com.tinqinacademy.authentication.restexport;
 import com.tinqinacademy.authentication.api.RouteExports;
 import com.tinqinacademy.authentication.api.operations.generateaccesstoken.GetUsernameFromTokenInput;
 import com.tinqinacademy.authentication.api.operations.generateaccesstoken.GetUsernameFromTokenOutput;
+import com.tinqinacademy.authentication.api.operations.getuser.GetUserOutput;
 import com.tinqinacademy.authentication.api.operations.loaduserdetails.LoadUserDetailsInput;
 import com.tinqinacademy.authentication.api.operations.loaduserdetails.LoadUserDetailsOutput;
 import com.tinqinacademy.authentication.api.operations.validateacesstoken.ValidateAccessTokenInput;
 import com.tinqinacademy.authentication.api.operations.validateacesstoken.ValidateAccessTokenOutput;
 import feign.Headers;
+import feign.Param;
 import feign.RequestLine;
 
 @Headers({"Content-Type: application/json"})
@@ -21,4 +23,7 @@ public interface AuthenticationClient {
 
     @RequestLine(RouteExports.LOAD_USER_DETAILS)
     LoadUserDetailsOutput loadUserDetails(LoadUserDetailsInput input);
+
+    @RequestLine(RouteExports.GET_USER_INFO)
+    GetUserOutput getUser(@Param String username);
 }
