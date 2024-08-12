850475232d20305e7517821324cd1ce902f2bbf2
moldovexc <alexanderparpulansky@gmail.com>
2024-08-12 19:09:22 +0300

    add pojos for resetting password

---
+++ api/src/main/java/com/tinqinacademy/authentication/api/operations/resetpassword/ResetPassword.java
@@ -0,0 +1,6 @@
+package com.tinqinacademy.authentication.api.operations.resetpassword;
+
+import com.tinqinacademy.authentication.api.base.OperationProcessor;
+
+public interface ResetPassword extends OperationProcessor<ResetPasswordInput, ResetPasswordOutput> {
+}
---
+++ api/src/main/java/com/tinqinacademy/authentication/api/operations/resetpassword/ResetPasswordOutput.java
@@ -0,0 +1,9 @@
+package com.tinqinacademy.authentication.api.operations.resetpassword;
+
+import com.tinqinacademy.authentication.api.base.OperationOutput;
+import lombok.Builder;
+import lombok.ToString;
+
+@Builder
+@ToString
+public class ResetPasswordOutput implements OperationOutput {}
