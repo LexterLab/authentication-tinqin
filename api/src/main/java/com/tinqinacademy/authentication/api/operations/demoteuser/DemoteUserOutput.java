d668187377934db6a2db4d2e39424b98a6ad0c31
moldovexc <alexanderparpulansky@gmail.com>
2024-08-13 12:09:25 +0300

    Add demote user op pojos

---
+++ api/src/main/java/com/tinqinacademy/authentication/api/operations/demoteuser/DemoteUserOutput.java
@@ -0,0 +1,10 @@
+package com.tinqinacademy.authentication.api.operations.demoteuser;
+
+import com.tinqinacademy.authentication.api.base.OperationOutput;
+import lombok.Builder;
+import lombok.ToString;
+
+@Builder
+@ToString
+public class DemoteUserOutput implements OperationOutput {
+}
