bbfb2cbe4487327d12ff5717900f56e61c3907cc
moldovexc <alexanderparpulansky@gmail.com>
2024-08-13 20:03:04 +0300

    Export find user by phone op to feign

--- api/src/main/java/com/tinqinacademy/authentication/api/RouteExports.java
+++ api/src/main/java/com/tinqinacademy/authentication/api/RouteExports.java
@@ -5,4 +5,5 @@ public class RouteExports {
     public static final String GET_USERNAME_FROM_TOKEN = "POST " + RestAPIRoutes.GET_USER_FROM_TOKEN;
     public static final String LOAD_USER_DETAILS = "POST " + RestAPIRoutes.LOAD_USER;
     public static final String GET_USER_INFO = "GET " + RestAPIRoutes.GET_USER;
+    public static final String FIND_USER = "GET " + RestAPIRoutes.FIND_USER;
 }
